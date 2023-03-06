2ded8145cecaf9845ce6135b93ac5f2b78f4dcee
vhlahti <henri.veholahti@gmail.com>
2023-03-06 11:08:05 +0200

    add assets content to templates with modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kahvila';
+  tarjoiluOhje: {tyo: string, poydanNumero: string, myyntiMaara: string}[] = [];
+  poydanNumero = '';
+  myyntiMaara = '';
+  faCoffee = faCoffee;
+
+  kahviaMyyty() {
+    this.tarjoiluOhje.push({
+      tyo: 'myyty',
+      poydanNumero: this.poydanNumero,
+      myyntiMaara: this.myyntiMaara
+    });
+  }
+
+  kahviaTarjoiltu() {
+    this.tarjoiluOhje.push({
+      tyo: 'tarjoiltu',
+      poydanNumero: this.poydanNumero,
+      myyntiMaara: this.myyntiMaara
+    });
+  }
 }
